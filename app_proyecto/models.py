3a449e47ebd1812de8d5846972708d801f8a3ed3
Clara Correa Luna <claracorrealu@gmail.com>
2022-07-07 18:15:37 -0300

    configuracion de herencia de templates,  models y forms

--- app_proyecto/models.py
+++ app_proyecto/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):
+    empresa = models.CharField(max_length=40)
+    mail = models.EmailField()
+    fecha_compra = models.DateField()
+
+class Vendedor(models.Model):
+    nombre = models.CharField(max_length=40)
+    apellido = models.CharField(max_length=40)
+    mail = models.EmailField()
+    antiguedad = models.IntegerField()
+
+class Productos(models.Model):
+    producto = models.CharField(max_length=40)
+    stock = models.IntegerField()
--- app_proyecto/views.py
+++ app_proyecto/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, "app_proyecto/home.html", {})
+def mostrar_inicio(request):
+    return render(request, "app_proyecto/base.html", {})
+
+def mostrar_clientes(request):
+    return render(request, "app_proyecto/clientes.html", {})
+
+
+
+def mostrar_vendedores(request):
+    return render(request, "app_proyecto/vendedores.html", {})
+
+def mostrar_productos(request):
+    return render(request, "app_proyecto/productos.html", {})
