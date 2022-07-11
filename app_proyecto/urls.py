30b2ee2f530f12f0d5d6e65c4f755337631e0464
Clara Correa Luna <claracorrealu@gmail.com>
2022-07-11 15:31:57 -0300

    con readme

--- app_proyecto/urls.py
+++ app_proyecto/urls.py
@@ -3,11 +3,11 @@ from app_proyecto.views import buscar, clienteForm, vendedorForm, productoForm, 
 
 urlpatterns = [
     path('inicio/', mostrar_inicio),
-    path('clientes/', clienteForm),
+    path('ingresar-cliente/', clienteForm),
     path('lista-clientes/', mostrar_clientes),
-    path('vendedor/', vendedorForm),
+    path('ingresar-vendedor/', vendedorForm),
     path('lista-vendedores/', mostrar_vendedores),
-    path('productos/', productoForm),
+    path('ingresar-producto/', productoForm),
     path('lista-productos/', mostrar_productos),
     path('buscar/', buscar),
 ]
