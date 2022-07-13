08ed0dacd1581014d38b2f248ccf80ee0a5a2e14
Clara Correa Luna <claracorrealu@gmail.com>
2022-07-13 20:47:45 -0300

    botones

--- app_proyecto/urls.py
+++ app_proyecto/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from app_proyecto.views import buscar, clienteForm, vendedorForm, productoForm, mostrar_clientes, mostrar_inicio, mostrar_vendedores, mostrar_productos
 
 urlpatterns = [
-    path('inicio/', mostrar_inicio),
-    path('ingresar-cliente/', clienteForm),
-    path('lista-clientes/', mostrar_clientes),
-    path('ingresar-vendedor/', vendedorForm),
-    path('lista-vendedores/', mostrar_vendedores),
-    path('ingresar-producto/', productoForm),
-    path('lista-productos/', mostrar_productos),
-    path('buscar/', buscar),
+    path('inicio/', mostrar_inicio, name = 'inicio'),
+    path('ingresar-cliente/', clienteForm, name = 'ingresar-cliente'),
+    path('lista-clientes/', mostrar_clientes, name = 'lista-clientes'),
+    path('ingresar-vendedor/', vendedorForm, name= 'ingresar-vendedor'),
+    path('lista-vendedores/', mostrar_vendedores, name = 'lista-vendedores'),
+    path('ingresar-producto/', productoForm, name= 'ingresar-producto'),
+    path('lista-productos/', mostrar_productos, name= 'lista-productos'),
+    path('buscar/', buscar, name = 'busqueda_productos'),
 ]
